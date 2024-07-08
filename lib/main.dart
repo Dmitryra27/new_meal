12fdbcaad9fe8600d9e0127ab11e70b55c49dbc1
Dmitryra27 <dmitryra27@gmail.com>
2024-07-08 13:07:28 +0300

    Исправлена проблема ключей - перед загрузкой на хостинг

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import '../arj/firebase_options.dart';
 
 
 Future<void> main() async {
